71fee6fbe4cf43441803413442175b4d1df0fece
Aidar Putilov <putiloff@gmail.com>
2024-07-02 03:31:28 +0500

    Edit settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 
@@ -18,5 +19,10 @@ ITEMS_PER_PAGE = 2
 
 # Количество страниц
 PAGES_OF_ITEMS = 10
+
+# Формат логов
+FORMAT = '%(asctime)-30s %(filename)-20s %(message)s'
+
+logging.basicConfig(level=logging.INFO, filename='main.log', filemode='w', format=FORMAT)
 
 #print(DATA_FILE_PATH)
